ac5a77374654ce81e8677db4255415e09d2c59c9
NoahWGe <63819881+NoahWGe@users.noreply.github.com>
2020-04-16 23:36:55 +0200

    Create view-page.vue

---
+++ view-page.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="another-page">
+    <page-splash text="This is a page"></page-splash>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  components: {
+    pageSplash: () => import("./splash.vue")
+  }
+});
+</script>
