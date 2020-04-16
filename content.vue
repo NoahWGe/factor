1c49ea235c685a937defd5d34216afda3c498b34
NoahWGe <63819881+NoahWGe@users.noreply.github.com>
2020-04-16 23:32:11 +0200

    Create content.vue

---
+++ content.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="content">
+    <page-header></page-header>
+    <router-view />
+  </div>
+</template>
+<script>
+export default {
+  components: {
+    pageHeader: () => import("./header.vue")
+  }
+};
+</script>
