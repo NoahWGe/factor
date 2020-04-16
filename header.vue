98dfa8044f3e77086bf8b16747f9827af8709feb
NoahWGe <63819881+NoahWGe@users.noreply.github.com>
2020-04-16 23:33:53 +0200

    Create header.vue

---
+++ header.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="app-header">
+    <factor-link path="/">Home</factor-link>
+    <factor-link path="/page">Page</factor-link>
+  </div>
+</template>
+<script>
+import Vue from "vue";
+import { factorLink } from "@factor/ui";
+export default Vue.extend({
+  components: {
+    factorLink
+  }
+});
+</script>
+<style lang="less">
+.app-header {
+  display: flex;
+  justify-content: center;
+  a {
+    margin: 1rem;
+  }
+}
+</style>
